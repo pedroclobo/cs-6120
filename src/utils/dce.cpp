3c10f219e1df176eee0b5a80391c5001f36a490e
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2025-03-02 18:47:24 +0000

    feat: fixed-point trivial dce pass

--- src/utils/dce.cpp
+++ src/utils/dce.cpp
@@ -8,20 +8,26 @@
 using Json = nlohmann::json;
 
 void LDCE::run(BasicBlock &bb) {
-  std::unordered_set<std::string> used;
+  bool changed = true;
+  while (changed) {
+    changed = false;
+    std::unordered_set<std::string> used;
 
-  for (size_t i = 0; i < bb.size(); ++i) {
-    const Json &instr = bb[i];
-    for (auto &arg : InstructionUtils::getArgs(instr))
-      used.insert(*arg);
-  }
+    for (size_t i = 0; i < bb.size(); ++i) {
+      const Json &instr = bb[i];
+      for (auto &arg : InstructionUtils::getArgs(instr))
+        used.insert(*arg);
+    }
 
-  for (int i = static_cast<int>(bb.size()) - 1; i >= 0; --i) {
-    const Json &instr = bb[static_cast<size_t>(i)];
-    if (InstructionUtils::hasDest(instr)) {
-      const auto &dest = InstructionUtils::getDest(instr);
-      if (used.find(dest) == used.end())
-        bb.removeInstruction(static_cast<size_t>(i));
+    for (int i = static_cast<int>(bb.size()) - 1; i >= 0; --i) {
+      const Json &instr = bb[static_cast<size_t>(i)];
+      if (InstructionUtils::hasDest(instr)) {
+        const auto &dest = InstructionUtils::getDest(instr);
+        if (used.find(dest) == used.end()) {
+          bb.removeInstruction(static_cast<size_t>(i));
+          changed = true;
+        }
+      }
     }
   }
 }
