b1e5e20993014ff913c955f5621249a042646221
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2025-03-02 19:05:19 +0000

    feat: remove dead declarations in dce pass

--- src/utils/dce.cpp
+++ src/utils/dce.cpp
@@ -12,6 +12,7 @@ void LDCE::run(BasicBlock &bb) {
   while (changed) {
     changed = false;
     std::unordered_set<std::string> used;
+    std::map<std::string, bool> seen_def;
 
     for (size_t i = 0; i < bb.size(); ++i) {
       const Json &instr = bb[i];
@@ -21,6 +22,19 @@ void LDCE::run(BasicBlock &bb) {
 
     for (int i = static_cast<int>(bb.size()) - 1; i >= 0; --i) {
       const Json &instr = bb[static_cast<size_t>(i)];
+      if (InstructionUtils::hasDest(instr)) {
+        if (seen_def[InstructionUtils::getDest(instr)]) {
+          // We've already seen a declaration for this variable that succeeds
+          // the current declaration. This is a dead declaration. Remove it.
+          seen_def[InstructionUtils::getDest(instr)] = false;
+          bb.removeInstruction(static_cast<size_t>(i));
+          changed = true;
+        } else {
+          // This is the first time we've seen a declaration for this variable.
+          seen_def[InstructionUtils::getDest(instr)] = true;
+        }
+      }
+
       if (InstructionUtils::hasDest(instr)) {
         const auto &dest = InstructionUtils::getDest(instr);
         if (used.find(dest) == used.end()) {
